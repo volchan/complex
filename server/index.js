2aba84c62daaaf49d9ba1bc434d89d01a5499eb9
stephane de la torre <s.dlt@hotmail.fr>
2020-02-27 18:54:39 +0100

    Docker 8/114 express and pg setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const keys = require("./keys");
+
+// Express App Setup
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
+
+pgClient.on("error", () => console.log("Lost PG connection"));
+
+pgClient
+  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+  .catch(err => console.log(err));
